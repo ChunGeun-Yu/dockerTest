44acfe03e1b85ad19c5866d26418b966754026df
cg <chungeun.yu@gmail.com>
2021-05-23 17:32:25 +0900

    change minor src code for docker hub automated build test

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 80;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.end('my test node app');
+    res.end('my test node app111');
 });
 
 server.listen(port, hostname, () => {
